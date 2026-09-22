7a1679cbdd2b63c85e3b794c58037501d6db65dd
Jilrotorua <rizza8autista@gmail.com>
2026-09-22 17:29:18 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jil-lifegroup-github-v44-coloured-settings-cards';
+const CACHE='jil-lifegroup-github-v45-backup-restore';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./config.js','./manifest.webmanifest','./bible-data.js','./daily-verses.js','./icon-180.png','./icon-192.png','./icon-512.png']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
